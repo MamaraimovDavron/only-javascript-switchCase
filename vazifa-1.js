d5ecc438d5cb179faeb26c94fed778fa487a96fa
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-05-04 11:44:14 +0500

    Vazifa-1 finished

---
+++ vazifa-1.js
@@ -0,0 +1,27 @@
+let baho = 1;
+
+switch(baho){
+    case 1:
+        console.log("Yomon!");
+        break;
+
+    case 2:
+        console.log("Qoniqarsiz!");
+        break;
+
+    case 3:
+        console.log("Qoniqarli!");
+        break;
+        
+    case 4:
+        console.log("Yaxshi!");
+        break;
+    
+    case 5:
+        console.log("A`lo!");
+        break;
+
+        default :
+        console.log("Bunday baho mavjud emas!");
+        break;
+}
